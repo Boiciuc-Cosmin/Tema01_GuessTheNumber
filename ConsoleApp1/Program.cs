5f6767830e0bd161bc0649e941c1462c4b5d58f5
Boiciuc Cosmin <cosmin.boiciuc9@gmail.com>
2019-11-28 18:07:13 +0200

    Program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nr=0;
+            Random rand = new Random();
+            int r = rand.Next(0, 100);
+           // Console.WriteLine("Numarul random generat {0}",r);
+            Console.WriteLine("Guess the number");
+            do
+            {
+                Console.Write("Introduceti un numar intre [0,100]: ");
+                string numar = Console.ReadLine();
+                nr = int.Parse(numar);
+
+                if (nr < r)
+                    Console.WriteLine("Numarul introdus este mai mic");
+                if (nr > r)
+                    Console.WriteLine("Numarul introdus este mai mare");
+            }
+            while (nr != r);
+                
+            Console.WriteLine("Ati ghicit numarul");
+          
+           
+        }
+    }
+}

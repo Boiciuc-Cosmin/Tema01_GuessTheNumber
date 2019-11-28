88bdc6aae412bb6f5d310a3bdeedb18755148bb6
Boiciuc Cosmin <cosmin.boiciuc9@gmail.com>
2019-11-28 18:18:42 +0200

    Final

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp1
             int nr=0;
             Random rand = new Random();
             int r = rand.Next(0, 100);
-           // Console.WriteLine("Numarul random generat {0}",r);
+            Console.WriteLine("Numarul random generat {0}",r);
             Console.WriteLine("Guess the number");
             do
             {
